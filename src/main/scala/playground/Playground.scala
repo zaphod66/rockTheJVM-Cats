f01c7e8e11b0d807c74e51ea9c560c3514a20feb
Norbert Scheller <norbert@riskident.com>
2021-11-19 16:46:55 +0100

    master - playground

---
+++ src/main/scala/playground/Playground.scala
@@ -0,0 +1,13 @@
+package playground
+
+import cats.Eval
+
+object Playground {
+  val meaningOfLife: Eval[Int] = Eval.later {
+    println("Learning Cats: computing abstractions and the meaning of life...")
+    42
+  }
+
+  def main(args: Array[String]): Unit =
+    println(meaningOfLife.value)
+}
