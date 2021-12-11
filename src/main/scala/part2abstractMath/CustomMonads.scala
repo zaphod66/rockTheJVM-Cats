81e604c94281be23a418d83d4dce60e51f7139b1
Norbert Scheller <norbert@riskident.com>
2021-12-11 12:41:45 +0100

    master - custom monads

---
+++ src/main/scala/part2abstractMath/CustomMonads.scala
@@ -0,0 +1,11 @@
+package part2abstractMath
+
+object Monads {
+
+  import cats.syntax.applicative._  // pure
+  import cats.syntax.flatMap._      // flatMap
+  
+  def main(args: Array[String]): Unit = {
+
+  }
+}
